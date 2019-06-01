0249bff7902f3042c1216b2c65e7276169cb05a6
Felipe <felipe@MacBook-Pro-de-Felipe.local>
2019-06-01 16:33:07 -0300

    Finalizacao dos estilos

--- src/components/form/index.js
+++ src/components/form/index.js
@@ -5,28 +5,37 @@ import "./style.css";
 
 export default class Form extends Component{
 
+    state = {
+        task: {
+            title: {},
+            description: {}
+        }
+    }
+
+    addTask(task) {
+        this.setState = { task };
+    }
+
+    
 
     render() {
         return (
             <div className="form-tarefa">
                 <form id="form-tarefa" className="form-group">
                     <div>
-                        <label>Título</label>
+                        <label>Título:</label>
                         <input type="text" id="titulo-tarefa" className="form-control form-control-lg"></input>
                     </div>
 
                     <div>
-                        <label>Descrição</label>
+                        <label>Descrição:</label>
                         <textarea type="text" id="descricao-tarefa" className="form-control form-control-lg"></textarea>
                     </div>
                     <div className="buttons">
-                        <Link to="/" className="btn btn-danger" >Cancelar</Link>
+                        <Link to="/" className="btn btn-cancelar" >Cancelar</Link>
 
-                        <Link to="/" className="btn btn-success btn-adicionar">Adicionar</Link>
+                        <Link to="/" className="btn btn-adicionar">Adicionar</Link>
                     </div>
-                    
-                    
-
                 </form>
             </div>
         );
